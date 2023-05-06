d11b9ff3597bae071b0e2f003a4308c1150ef6cb
Krishna Lagad <krishnalagad2@gmail.com>
2023-05-06 21:46:02 +0530

    - Project created.

---
+++ TodoManager/src/test/kotlin/com/todo/manager/TodoManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.todo.manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
