f83c5834515438fe25137246c6b0c32b52ebd423
Krishna Lagad <krishnalagad2@gmail.com>
2023-05-07 17:34:35 +0530

    - dump components creation.

--- todo-manager/src/app/app-routing.module.ts
+++ todo-manager/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { ViewTodosComponent } from './pages/view-todos/view-todos.component';
+import { UpdateTodoComponent } from './pages/update-todo/update-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'add-todo', component: AddTodoComponent },
+  { path: 'view-todos', component: ViewTodosComponent },
+  { path: 'update-todo', component: UpdateTodoComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- todo-manager/src/app/app.module.ts
+++ todo-manager/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './pages/home/home.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { ViewTodosComponent } from './pages/view-todos/view-todos.component';
+import { UpdateTodoComponent } from './pages/update-todo/update-todo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddTodoComponent,
+    ViewTodosComponent,
+    UpdateTodoComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
