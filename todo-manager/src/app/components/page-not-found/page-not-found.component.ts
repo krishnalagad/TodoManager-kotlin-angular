e6962c2b848664b8243999b069ff00f2f9c57358
Krishna Lagad <krishnalagad2@gmail.com>
2023-05-11 18:02:52 +0530

    - dump get user's location code.

--- todo-manager/src/app/components/page-not-found/page-not-found.component.ts
+++ todo-manager/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.css'],
 })
 export class PageNotFoundComponent {
+  constructor(private _router: Router) {}
+  getLocation() {
+    // alert("not found")
 
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitute = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitute}&lon=${longitude}`;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.table(data.address);
+        })
+        .catch(() => {
+          console.log('Error fetching data from API.');
+        });
+    });
+    this._router.navigate(['/'])
+  }
 }
