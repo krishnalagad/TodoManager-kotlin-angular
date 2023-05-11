968c402fb8becb20d5854df0bb2a25ef2a8ffb79
Krishna Lagad <krishnalagad2@gmail.com>
2023-05-11 18:03:04 +0530

    Update page-not-found.component.ts

--- todo-manager/src/app/components/page-not-found/page-not-found.component.ts
+++ todo-manager/src/app/components/page-not-found/page-not-found.component.ts
@@ -9,7 +9,6 @@ import { Router } from '@angular/router';
 export class PageNotFoundComponent {
   constructor(private _router: Router) {}
   getLocation() {
-    // alert("not found")
 
     navigator.geolocation.getCurrentPosition((position) => {
       const latitute = position.coords.latitude;
